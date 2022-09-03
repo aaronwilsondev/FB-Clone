0730d8f6b09a1f8b65302620d5f936ee5353a1f3
Aaron Wilson <aaronwilson1234@outlook.ie>
2022-09-03 21:32:52 +0100

    finishing function for sending verification email

--- backend/helpers/mailer.js
+++ backend/helpers/mailer.js
@@ -4,36 +4,39 @@ const { google } = require("googleapis");
 
 const { OAuth2 } = google.auth;
 const oauth_link = "https://developers.google.com/oauthplayground";
-const { EMAIL, MAILING_ID, MAILING_REFRESH, MAILING_SECRET } = process.env;
+// const { EMAIL, MAILING_ID, MAILING_REFRESH, MAILING_SECRET } = process.env.;
 
 const auth = new OAuth2(
-  MAILING_ID,
-  MAILING_SECRET,
-  MAILING_REFRESH,
-  oauth_link
+  process.env.MAILING_ID,
+  process.env.MAILING_SECRET,
+  process.env.MAILING_REFRESH,
+  process.env.oauth_link
 );
 
 exports.sendVerificationEmail = (email, name, url) => {
   auth.setCredentials({
-    refresh_token: MAILING_REFRESH,
+    refresh_token: process.env.MAILING_REFRESH,
   });
   const accessToken = auth.getAccessToken();
   const stmp = nodemailer.createTransport({
     service: "gmail",
     auth: {
       type: "OAuth2",
-      user: EMAIL,
-      clientId: MAILING_ID,
-      clientSecret: MAILING_SECRET,
-      refreshToken: MAILING_REFRESH,
+      user: process.env.EMAIL,
+      clientId: process.env.MAILING_ID,
+      clientSecret: process.env.MAILING_SECRET,
+      refreshToken: process.env.MAILING_REFRESH,
       accessToken,
+    },
+    tls: {
+      rejectUnauthorized: false,
     },
   });
   const mailOptions = {
-    from: EMAIL,
+    from: process.env.EMAIL,
     to: email,
     subject: "Facebook email verification, verify email to procced",
-    html: "",
+    html: `<div style="max-width:700px;margin-bottom:1rem;display:flex;align-items:center;gap:10px;font-family:Roboto;font-weight:600;color:#3b5990"><img src="https://res.cloudinary.com/dmhcnhtng/image/upload/v1645134414/logo_cs1si5.png" alt=""><span>Action required : Activate your Facebook account</span></div><div style="padding:1rem 0;border-top:1px solid #e5e5e5;border-bottom:1px solid #e5e5e5;color:#141823;font-size:17px;font-family:Robunto"><span>Hello ${name}</span><div style="padding:20px 0"><span style="padding:1.5rem 0">You recently created an account on Facebook. To complete your registration. Please confirm your account</span></div><a href=${url} style="width:200px;padding:10px 15px;background:#4c649b;color:#fff;text-decoration:none;font-weight:600">Confirm your account</a><br><div style="padding-top:20px"><span style="margin:1.5rem;color:#898f9c">Facebook allows you to stay in touch with all you friends. Once registered on facebook, you can share, organise and much more.</span></div></div>`,
   };
   stmp.sendMail(mailOptions, (err, res) => {
     if (err) return err;
