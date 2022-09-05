b37726375169033647b741c4c3e7839d80fc613c
Aaron Wilson <aaronwilson1234@outlook.ie>
2022-09-05 21:30:29 +0100

    creating and styling login page

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,5 +1,46 @@
+import "./styles.css";
 import React from "react";
+import { Formik, Form } from "formik";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="login_wrapper">
+        <div className="login_wrap">
+          <div className="login_1">
+            <img src="../../icons/facebook.svg" alt="" />
+            <span>
+              Facebook helps you connect and share with the people in you life
+            </span>
+          </div>
+          <div className="login_2">
+            <div className="login_2_wrap">
+              <Formik>
+                {(formik) => (
+                  <Form>
+                    <input type="text" />
+                    <input type="text" />
+                    <button type="submit" className="blu_btn">
+                      Log In
+                    </button>
+                  </Form>
+                )}
+              </Formik>
+              <Link to="/forgot" className="forgot_password">
+                Forgotten Password
+              </Link>
+              <div className="sign_splitter"></div>
+              <button className="blu_btn open_signup">Create Account</button>
+            </div>
+            <Link to="/" className="sign_extra">
+              <b>Create a Page </b>
+              for a celebrity, brand or buisness
+            </Link>
+          </div>
+        </div>
+        <div className="register"></div>
+      </div>
+    </div>
+  );
 }
